c61fa297a70f43037eaef98c293c3b3d576af8c1
James Lawlor <jalawlor@tcd.ie>
2017-06-10 13:55:30 +0100

    updated football results scraper

--- football_data_co_uk_scraper.py
+++ football_data_co_uk_scraper.py
@@ -6,23 +6,64 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
+import numpy as np
 
-base_url = "http://football-data.co.uk/mmz4281/" #1617/E0.csv
+data_path = "./data/"
+url_template = "http://football-data.co.uk/mmz4281/{season}/{league}.csv"
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-dic = {"england": {
-                "code" : "E",   # used in URL
-		"start_year" : 2004,
+dic = {
+#        "england": {
+#                "code" : "E",   # used in URL
+#		"start_year" : 2009,
+#		"end_year" : 2016,
+#		"leagues" : [
+#                            ("0","prem"),  # (url_required, human_readable)
+#			    ("1","champ"),
+#			    ("2","league_1"),
+#			    ("3","league_2"),
+#		#	    ("C","conference" )
+#			    ],
+#	},
+#        "germany":{
+#                "code" : "D",
+#		"start_year" : 2009,
+#		"end_year" : 2016,
+#		"leagues" : [
+#                            ("1","bundes_1"),  # (url_required, human_readable)
+#			    ("2","bundes_2"),
+#                            ],
+#        },
+        "france":{
+                "code" : "F",
+		"start_year" : 2015,
 		"end_year" : 2016,
-		"leagues" : [("0","prem"),  # (url_required, human_readable)
-				("1","champ"),
-				("2","league_1"),
-				("3","league_2"),
-		#		("C","conference" )
-			],
-	},
-        #"france":{},
+		"leagues" : [
+                            ("1","le_championnat"),  # (url_required, human_readable)
+			    ("2","div_2"),
+                            ],
+        },
+#        "italy":{
+#                "code" : "I",
+#		"start_year" : 2009,
+#		"end_year" : 2016,
+#		"leagues" : [
+#                            ("1","serie_a"),  # (url_required, human_readable)
+#			    ("2","serie_b"),
+#                            ],
+#        },
+#        "spain":{
+#                "code" : "SP",
+#		"start_year" : 2009,
+#		"end_year" : 2016,
+#		"leagues" : [
+#                            ("1","la_liga_1"),  # (url_required, human_readable)
+#			    ("2","la_liga_2"),
+#                            ],
+#        },
 }
 
 with requests.Session() as s:
@@ -38,19 +79,21 @@ with requests.Session() as s:
         
         for season in seasons:
             for league in leagues:
-                league_url = league[0]
-                url = base_url +  season + "/" + code+league[0] 
+                league_url = code+league[0]
+                url = url_template.format(season=season,league=league_url)
                 print(url)
 
                 league_readable = league[1]
                 filename = league_readable + "_" + season + ".csv"
-                
+
+#                df = pd.read_csv(url,parse_dates=True,encoding="latin1")
+#                print(df.head())
                 s.headers.update(headers)
                 download = s.get(url)
                 decoded_content = download.text
                 cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-                with open(filename,"w") as f:
+                with open(data_path+filename,"w") as f:
                     writer = csv.writer(f)
                     for row in cr:
                         writer.writerow(row)
-                time.sleep(1)
+                time.sleep(np.random.uniform(low=5.0, high=15.0, size=None))
